6f3a703d7dcdaf966309f1879d34be2e926795b4
Samir Hilali <samir.hilali@epitech.eu>
2024-01-18 14:28:47 +0100

    fix validation price

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -7,8 +7,7 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
     public class ProductViewModel
     {
-        [BindNever]
-        public int Id { get; init; }
+        [BindNever] public int Id { get; init; }
 
         [LocalizedRequired<ProductService>("MissingName")]
         public string Name { get; init; }
@@ -20,13 +19,15 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         public string Details { get; init; }
 
         [LocalizedRequired<ProductService>("MissingStock")]
-        [Range(1, int.MaxValue, ErrorMessageResourceName = "StockNotGreaterThanZero", ErrorMessageResourceType = typeof(Ressource.ProductService))]
-        [RegularExpression(@"^\d+$", ErrorMessageResourceName = "StockNotAnInteger", ErrorMessageResourceType = typeof(Ressource.ProductService))]
+        [Range(1, int.MaxValue, ErrorMessageResourceName = "StockNotGreaterThanZero",
+            ErrorMessageResourceType = typeof(Ressource.ProductService))]
+        [RegularExpression(@"^\d+$", ErrorMessageResourceName = "StockNotAnInteger",
+            ErrorMessageResourceType = typeof(Ressource.ProductService))]
         public string Stock { get; init; }
 
         [LocalizedRequired<ProductService>("MissingPrice")]
-        [Range(0.01, double.MaxValue, ErrorMessageResourceName = "PriceNotGreaterThanZero", ErrorMessageResourceType = typeof(Ressource.ProductService))]
-        [RegularExpression(@"^\d+$", ErrorMessageResourceName = "PriceNotANumber", ErrorMessageResourceType = typeof(Ressource.ProductService))]
+        [RegularExpression(@"^[0-9]*([.,][0-9]+)?$", ErrorMessageResourceName = "PriceNotANumber",
+            ErrorMessageResourceType = typeof(Ressource.ProductService))]
         public string Price { get; init; }
     }
 }
