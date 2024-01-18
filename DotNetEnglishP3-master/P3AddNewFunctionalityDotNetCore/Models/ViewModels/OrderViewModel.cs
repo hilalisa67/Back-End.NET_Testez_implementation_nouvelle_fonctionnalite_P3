dc059cfcc42dc3b5490d034f1ce58ed9910eea91
Samir Hilali <samir.hilali@epitech.eu>
2024-01-18 10:01:08 +0100

    add validation controls

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/OrderViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/OrderViewModel.cs
@@ -1,34 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using P3AddNewFunctionalityDotNetCore.Resources.Models.ViewModels;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
     public class OrderViewModel
     {
         [BindNever]
-        public int OrderId { get; set; }
+        public int OrderId { get; init; }
 
         [BindNever]
         public ICollection<CartLine> Lines { get; set; }
+        
+        [LocalizedRequired<Order>("ErrorMissingName")]
+        public string Name { get; init; }
 
-        [Required(ErrorMessage = "ErrorMissingName")]
-        public string Name { get; set; }
+        [LocalizedRequired<Order>("ErrorMissingAddress")]
+        public string Address { get; init; }
 
-        [Required(ErrorMessage = "ErrorMissingAddress")]
-        public string Address { get; set; }
+        [LocalizedRequired<Order>("ErrorMissingCity")]
+        public string City { get; init; }
 
-        [Required(ErrorMessage = "ErrorMissingCity")]
-        public string City { get; set; }
+        [LocalizedRequired<Order>("ErrorMissingZipCode")]
+        public string Zip { get; init; }
 
-        [Required(ErrorMessage = "ErrorMissingZipCode")]
-        public string Zip { get; set; }
-
-        [Required(ErrorMessage = "ErrorMissingCountry")]
-        public string Country { get; set; }
+        [LocalizedRequired<Order>("ErrorMissingCountry")]
+        public string Country { get; init; }
 
         [BindNever]
-        public DateTime Date { get; set; }
+        public DateTime Date { get; init; }
     }
 }
